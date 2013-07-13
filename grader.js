328d376f2d5d3a179df5ff416066b35cff9463ff
gxpj77 <gxpj77@gmail.com>
2013-07-13 05:28:31 +0000

    Add url option to grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var restler = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +66,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to validate')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+        restler.get(program.url).on('complete', function(urlContent) {
+		fs.writeFileSync("file.html", urlContent);
+                var checkJson = checkHtmlFile("file.html", program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
